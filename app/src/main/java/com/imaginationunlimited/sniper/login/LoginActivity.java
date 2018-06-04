c664ac101a5579cbd7d3cc85f8eeb7a9ea2e9ea4
mengshuangchun <mengshuangchun@163.com>
2018-06-04 16:58:57 +0800

    commit

---
+++ app/src/main/java/com/imaginationunlimited/sniper/login/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.imaginationunlimited.sniper.login;
+
+import com.imaginationunlimited.sniper.base.BaseActivity;
+
+/**
+ * Created by MSC on 2018/6/4.
+ * BJ AlphaMobile
+ */
+public class LoginActivity extends BaseActivity {
+}
---
+++ app/src/main/java/com/imaginationunlimited/sniper/module/UserInfo.java
@@ -0,0 +1,13 @@
+package com.imaginationunlimited.sniper.module;
+
+/**
+ * Created by MSC on 2018/6/4.
+ * BJ AlphaMobile
+ */
+
+/**
+ * user information
+ */
+public class UserInfo {
+
+}
