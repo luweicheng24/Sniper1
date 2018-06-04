cd62974bb6e37e904f5e1bd4d3cc9acbd7ee6c73
mengshuangchun <mengshuangchun@163.com>
2018-06-04 16:30:42 +0800

    commit

--- app/src/main/java/com/imaginationunlimited/sniper/chat/ChatActivity.java
+++ app/src/main/java/com/imaginationunlimited/sniper/chat/ChatActivity.java
@@ -1,19 +1,18 @@
-package com.imaginationunlimited.sniper.logo;
+package com.imaginationunlimited.sniper.chat;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
-import com.imaginationunlimited.sniper.R;
 import com.imaginationunlimited.sniper.base.BaseActivity;
 
 /**
  * Created by MSC on 2018/6/4.
  * BJ AlphaMobile
  */
-public class LogoActivity extends BaseActivity {
+public class ChatActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logo);
+
     }
 }
