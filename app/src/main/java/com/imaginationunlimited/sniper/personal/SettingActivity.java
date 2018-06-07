08ce40e875d7c631cab6b36b1a5d61a943b90c29
gengchunjiang <276410363@qq.com>
2018-06-07 17:49:56 +0800

    add my profile.

---
+++ app/src/main/java/com/imaginationunlimited/sniper/personal/SettingActivity.java
@@ -0,0 +1,32 @@
+
+package com.imaginationunlimited.sniper.personal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.imaginationunlimited.sniper.R;
+import com.imaginationunlimited.sniper.base.BaseActivity;
+
+public class SettingActivity extends BaseActivity {
+
+
+    @Override
+    protected void afterViewFound() {
+
+    }
+
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.activity_setting);
+    }
+
+    @Override
+    public void setUpPresenter() {
+
+    }
+
+    @Override
+    public void findViews() {
+
+    }
+}
